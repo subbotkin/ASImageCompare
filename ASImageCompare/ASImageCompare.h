133e1fcd0711fb35f7cdc5cb2a4c609cc8e09d90
Alexey Subbotkin <subbotkin147@gmail.com>
2016-12-19 21:55:25 +0200

    Initial project

---
+++ ASImageCompare/ASImageCompare.h
@@ -0,0 +1,19 @@
+//
+//  ASImageCompare.h
+//  ASImageCompare
+//
+//  Created by Alexey Subbotkin on 19/12/2016.
+//  Copyright © 2016 Alexey Subbotkin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ASImageCompare.
+FOUNDATION_EXPORT double ASImageCompareVersionNumber;
+
+//! Project version string for ASImageCompare.
+FOUNDATION_EXPORT const unsigned char ASImageCompareVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ASImageCompare/PublicHeader.h>
+
+
